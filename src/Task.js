e812bd022b2912275039488e713029c19d73b532
Will Baxter <bax.will@googlemail.com>
2024-06-06 10:34:04 +0100

    Refactored for better abstraction

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,22 @@
 class Task {
   constructor(dateDue, description) {
-    //Task due date - not all tasks have a due date. If a task has no
-    //due date, dueDate will be null
-    this.dateDue = dateDue
-    this.description = description
-    this.status = "incomplete"
+    this.dateDue = dateDue;
+    this.description = description;
+    this.status = "incomplete";
+  }
+
+  isIncomplete() {
+    return this.status === "incomplete";
+  }
+
+  hasDueDate() {
+    return this.dateDue !== null
+  }
+
+  isOverdue() {
+    const today = new Date();
+    return (this.isIncomplete() && this.hasDueDate() && today > this.dateDue);
   }
 }
 
-module.exports = Task
+module.exports = Task;
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,31 +1,22 @@
 class TaskList {
-
   constructor() {
-    this.tasks = []
+    this.tasks = [];
   }
 
   addTask(task) {
-    this.tasks.push(task)
+    this.tasks.push(task);
   }
 
   getOverdueTasks() {
-    const today = new Date()
-    const overdueTasks = []
+    const overdueTasks = [];
+
     for (const task of this.tasks) {
-      //it's not been completed
-      if (task.status === "incomplete") {
-        //if it has a due date
-        if (task.dateDue !== null) {
-          //if the due date has passed then the task is overdue
-          if (today > task.dateDue) {
-            overdueTasks.push(task)
-          }
-        }
+      if (task.isOverdue()) {
+        overdueTasks.push(task);
       }
     }
-
-    return overdueTasks
+    return overdueTasks;
   }
 }
 
-module.exports = TaskList
+module.exports = TaskList;
